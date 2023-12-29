5a916494cefde1b08c0a09bc83c9e8a72754be38
Ahmad Tahir <tahirahma12@gmail.com>
2023-12-29 18:16:48 +0100

    .

--- accomodation_review/accomodation_review/settings.py
+++ accomodation_review/accomodation_review/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,"static")]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,"static")
+MEDIA_ROOT = os.path.join(BASE_DIR,"media")
 
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
