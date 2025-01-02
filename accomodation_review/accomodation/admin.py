bb95993059c5964e1e48b7dafc2c5b475b9891db
Ahmad <114881687+tahirahmad23@users.noreply.github.com>
2025-01-02 15:37:35 +0100

    Delete accomodation_review directory

--- accomodation_review/accomodation/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import House,Photo,Review
-# Register your models here.
-admin.site.register(House)
-admin.site.register(Photo)
-admin.site.register(Review)
